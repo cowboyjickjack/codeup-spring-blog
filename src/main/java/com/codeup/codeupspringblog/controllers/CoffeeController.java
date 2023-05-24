271cb4b13bb030fd83f54fb1ff5369420f0c5d67
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-24 15:12:24 -0500

    continuing the view lecture

--- src/main/java/com/codeup/codeupspringblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/CoffeeController.java
@@ -1,9 +1,13 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.models.Coffee;
+import org.apache.naming.factory.SendMailFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CoffeeController {
@@ -13,9 +17,32 @@ public class CoffeeController {
         return "coffee";
     }
 
+    @PostMapping("/coffee")
+    public String newsletterSignUp(@RequestParam(name = "email") String email, Model model){
+        model.addAttribute("email", email);
+        return "coffee";
+    }
+
     @GetMapping("/coffee/{roast}")
     public String favRoast(@PathVariable String roast, Model model){
-        model.addAttribute("roast", roast);
+        // get used to idea that Controllers turn everything into Objects
+        // so normalize using objects
+        Coffee coffee1 = new Coffee(roast, "", "Cool Beans");
+        Coffee coffee2 = new Coffee(roast, "", "Java Beans");
+        // ternary version:
+        // coffee.setOrigin(roast.equals("dark") ? "Sumatra" : "Columbia");
+        if (roast.equals("dark")) {
+            coffee1.setOrigin("Sumatra");
+            coffee2.setOrigin("Brazil");
+        } else if (roast.equals("medium")){
+            coffee1.setOrigin("Columbia");
+            coffee2.setOrigin("Uganda");
+        } else {
+            coffee1.setOrigin("Chicago");
+            coffee2.setOrigin("Ethiopia");
+        }
+        // then we make a list!
+        model.addAttribute("coffee", coffee1);
         return "coffee";
     }
 }
