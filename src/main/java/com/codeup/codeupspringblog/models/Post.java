dcad0b852276f8072b27f3c589891a019491ee1d
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-31 09:18:19 -0500

    updating relationships lecture

--- src/main/java/com/codeup/codeupspringblog/models/Post.java
+++ src/main/java/com/codeup/codeupspringblog/models/Post.java
@@ -1,17 +1,28 @@
 package com.codeup.codeupspringblog.models;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="posts")
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length=100)
     private String title;
 
+    @Column(nullable = false, length=500)
     private String body;
 
-    public Post() {
+    public long getId() {
+        return id;
     }
 
-    public Post(String title, String body) {
-        this.title = title;
-        this.body = body;
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -27,6 +38,20 @@ public class Post {
     }
 
     public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Post() {
+    }
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
         this.body = body;
     }
 }
